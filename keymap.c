dd8dd9c457d433df421ad43125e9837ad574a05d
Shigenobu Nishikawa <shishi@srevo.net>
2022-07-27 03:22:46 +0900

    swap layer 1 and 2

--- keymap.c
+++ keymap.c
@@ -102,24 +102,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(2)    , KC_1         , KC_2         , KC_3         , KC_4         , KC_5         ,                                                               KC_6          , KC_7         , KC_8         , KC_9         , KC_0            , TO(3)         ,
     KC_JYEN  , KC_Q         , KC_W         , KC_E         , TD(TD_R)     , KC_T         ,                                                               KC_Y          , KC_U         , KC_I         , KC_O         , KC_P            , KC_GRV        ,
     KC_MINUS , LGUI_T(KC_A) , LALT_T(KC_S) , LSFT_T(KC_D) , LCTL_T(KC_F) , KC_G         ,                                                               KC_H          , RCTL_T(KC_J) , RSFT_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_SCLN) , LT(2,KC_QUOT) ,
-    KC_EQL   , TD(TD_Z)     , TD(TD_X)     , TD(TD_C)     , TD(TD_V)     , KC_B         , TD(TD_TRIPLE_BRACE_START) ,          TD(TD_TRIPLE_BRACE_END) , KC_N          , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH         , KC_BSLS       ,
+    KC_EQL   , TD(TD_Z)     , TD(TD_X)     , TD(TD_C)     , TD(TD_V)     , KC_B         , TD(TD_TRIPLE_BRACE_START) ,          TD(TD_TRIPLE_BRACE_END) , KC_N          , KC_M         , KC_COMM      , KC_DOT       , KC_SLSH         , KC_BSLS      ,
     KC_MEH   , KC_APP       , XXXXXXX      , XXXXXXX      , LT(3,KC_ESC) , LT(2,KC_SPC) , LT(1,KC_TAB)              ,          LT(1,KC_ENTER)          , LT(2,KC_BSPC) , XXXXXXX      , XXXXXXX      , XXXXXXX      , KC_PSCR         , KC_MEH
   ),
 
   [1] = LAYOUT_universal(
-    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                XXXXXXX , KC_INS   , KC_SCRL , XXXXXXX  , XXXXXXX , XXXXXXX ,
-    KC_LALT , KC_F1   , KC_F2   , KC_F3   , KC_F4   , XXXXXXX ,                                XXXXXXX , KC_HOME  , KC_END  , XXXXXXX  , XXXXXXX , KC_RALT ,
-    KC_LCTL , KC_F5   , KC_F6   , KC_F7   , KC_F8   , XXXXXXX ,                                KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , XXXXXXX , KC_RCTL ,
-    KC_LSFT , KC_F9   , KC_F10  , KC_F11  , KC_F12  , XXXXXXX , XXXXXXX ,            XXXXXXX , XXXXXXX , KC_PGUP  , KC_PGDN , XXXXXXX  , XXXXXXX , KC_RSFT ,
-    KC_LGUI , KC_F13  , KC_F14  , KC_F15  , _______ , _______ , _______ ,            _______ , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX , KC_RGUI
-  ),
-
-  [2] = LAYOUT_universal(
     TO(0)   , XXXXXXX  , KC_PEQL , KC_PSLS , KC_PAST , KC_NUM  ,                                  XXXXXXX    , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , TO(0)   ,
     KC_LALT , XXXXXXX  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                                  XXXXXXX    , KC_HOME , KC_END  , XXXXXXX , XXXXXXX , KC_RALT ,
     KC_LCTL , XXXXXXX  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                                  C(KC_W)    , KC_BTN1 , KC_BTN3 , KC_BTN2 , XXXXXXX , KC_RCTL ,
     KC_LSFT , XXXXXXX  , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX  ,            XXXXXXX  , C(S(KC_T)) , _______ , _______ , _______ , _______ , KC_RSFT ,
     KC_LGUI , XXXXXXX  , KC_0    , KC_PDOT , KC_PCMM , _______ , _______  ,            _______  , KC_DEL     , _______ , _______ , _______ , _______ , KC_RGUI
+  ),
+
+  [2] = LAYOUT_universal(
+    TO(0)   , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                XXXXXXX , KC_INS   , KC_SCRL , XXXXXXX  , XXXXXXX , TO(0)   ,
+    KC_LALT , KC_F1   , KC_F2   , KC_F3   , KC_F4   , XXXXXXX ,                                XXXXXXX , KC_HOME  , KC_END  , XXXXXXX  , XXXXXXX , KC_RALT ,
+    KC_LCTL , KC_F5   , KC_F6   , KC_F7   , KC_F8   , XXXXXXX ,                                KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , XXXXXXX , KC_RCTL ,
+    KC_LSFT , KC_F9   , KC_F10  , KC_F11  , KC_F12  , XXXXXXX , XXXXXXX ,            XXXXXXX , XXXXXXX , KC_PGUP  , KC_PGDN , XXXXXXX  , XXXXXXX , KC_RSFT ,
+    KC_LGUI , KC_F13  , KC_F14  , KC_F15  , _______ , _______ , _______ ,            _______ , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX , KC_RGUI
   ),
 
   [3] = LAYOUT_universal(
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 1);
+    keyball_set_scroll_mode(get_highest_layer(state) == 2);
     return state;
 }
 
