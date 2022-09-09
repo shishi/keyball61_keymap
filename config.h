cbd96398de3fc8d2eadf3604d6e513d2891bb2c6
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-09 14:11:54 +0900

    tapping force hold

--- config.h
+++ config.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 175
 #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
