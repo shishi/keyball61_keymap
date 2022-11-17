8e1ce1779dee791fae2a578c3331c51b4f58262b
Shigenobu Nishikawa <shishi@srevo.net>
2022-11-17 18:05:43 +0900

    space to L3

--- keymap.c
+++ keymap.c
@@ -134,8 +134,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(0)   , XXXXXXX  , KC_PEQL , KC_PSLS , KC_PAST , KC_NUM  ,                                  XXXXXXX    , XXXXXXX    , XXXXXXX     , XXXXXXX , XXXXXXX , TO(0)   ,
     KC_LALT , XXXXXXX  , KC_P7   , KC_P8   , KC_P9   , KC_PMNS ,                                  XXXXXXX    , A(KC_LEFT) , A(KC_RIGHT) , XXXXXXX , XXXXXXX , KC_RALT ,
     KC_LCTL , XXXXXXX  , KC_P4   , KC_P5   , KC_P6   , KC_PPLS ,                                  C(KC_W)    , KC_BTN1    , KC_BTN3     , KC_BTN2 , XXXXXXX , KC_RCTL ,
-    KC_LSFT , XXXXXXX  , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX  ,            XXXXXXX  , C(S(KC_T)) , KC_PGUP    , KC_PGDN     , XXXXXXX , XXXXXXX , KC_RSFT ,
-    KC_LGUI , XXXXXXX  , KC_0    , KC_PDOT , KC_PCMM , _______ , _______  ,            _______  , KC_DEL     , XXXXXXX    , XXXXXXX     , XXXXXXX , XXXXXXX , KC_RGUI
+    KC_LSFT , XXXXXXX  , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX ,            XXXXXXX  , C(S(KC_T)) , KC_PGUP    , KC_PGDN     , XXXXXXX , XXXXXXX , KC_RSFT ,
+    KC_LGUI , XXXXXXX  , KC_0    , KC_PDOT , KC_PCMM , XXXXXXX , XXXXXXX  ,           XXXXXXX  , KC_DEL     , XXXXXXX    , XXXXXXX     , XXXXXXX , XXXXXXX , KC_RGUI
   ),
 
   [2] = LAYOUT_universal(
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LALT , KC_F1   , KC_F2   , KC_F3   , KC_F4   , XXXXXXX ,                                XXXXXXX , KC_HOME  , KC_END  , XXXXXXX  , XXXXXXX , KC_RALT ,
     KC_LCTL , KC_F5   , KC_F6   , KC_F7   , KC_F8   , XXXXXXX ,                                KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , XXXXXXX , KC_RCTL ,
     KC_LSFT , KC_F9   , KC_F10  , KC_F11  , KC_F12  , XXXXXXX , XXXXXXX ,            XXXXXXX , XXXXXXX , KC_PGUP  , KC_PGDN , XXXXXXX  , XXXXXXX , KC_RSFT ,
-    KC_LGUI , KC_F13  , KC_F14  , KC_F15  , _______ , _______ , _______ ,            _______ , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX , KC_RGUI
+    KC_LGUI , KC_F13  , KC_F14  , KC_F15  , XXXXXXX , XXXXXXX , XXXXXXX ,            XXXXXXX , KC_DEL  , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX , KC_RGUI
   ),
 
   [3] = LAYOUT_universal(
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX , KBC_RST  , XXXXXXX , CPI_D1K  , CPI_I1K  , XXXXXXX  ,                              XXXXXXX , KC_LBRC    , KC_RBRC    , XXXXXXX , XXXXXXX , XXXXXXX ,
     XXXXXXX , KBC_SAVE , XXXXXXX , CPI_D100 , CPI_I100 , XXXXXXX  ,                              XXXXXXX , S(KC_9)    , S(KC_0)    , XXXXXXX , XXXXXXX , XXXXXXX ,
     XXXXXXX , XXXXXXX  , XXXXXXX , SCRL_DVI , SCRL_DVD , XXXXXXX  , RESET ,            EEP_RST , XXXXXXX , S(KC_LBRC) , S(KC_RBRC) , XXXXXXX , XXXXXXX , XXXXXXX ,
-    RESET   , XXXXXXX  , XXXXXXX , XXXXXXX  , _______  , _______  , _______ ,          _______ , KC_DEL  , XXXXXXX    , XXXXXXX    , XXXXXXX , XXXXXXX , XXXXXXX
+    RESET   , XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX  , KC_SPC , XXXXXXX ,          XXXXXXX , KC_DEL  , XXXXXXX    , XXXXXXX    , XXXXXXX , XXXXXXX , XXXXXXX
   ),
 };
 // clang-format on
